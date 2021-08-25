db1c5b6ebbeccfaf43040aa608939c30c0581b63
Benjamin Chalke <benchalke@hotmail.com>
2021-08-26 04:11:20 +1000

    Upgraded render speed and style

--- components/ImageUpload/ImageUpload.tsx
+++ components/ImageUpload/ImageUpload.tsx
@@ -9,9 +9,24 @@ interface SvgArray {
   y:number
 }
 
+interface ModifiedImageData {
+  (
+    data:Uint8ClampedArray,
+    width:number,
+    height:number,
+    x?:number,
+    y?:number
+  ):void
+};
+
 interface Prop {
   stdTileWidth:string,  
   stdTileHeight:string,
+}
+
+interface SvgImage extends HTMLImageElement {
+  svgX?:number,
+  svgY?:number,
 }
 
 const ImageUpload: React.FC<Prop> = ({ stdTileWidth, stdTileHeight }) => {
@@ -24,23 +39,21 @@ const ImageUpload: React.FC<Prop> = ({ stdTileWidth, stdTileHeight }) => {
   const [tileHeight, setTileHeight] = useState<number>();
   const [tileCountX, setTileCountX] = useState<number | undefined>();
   const [tileCountY, setTileCountY] = useState<number | undefined>();
-  const [imgData, setImgData] = useState<any>();
+  const [imgData, setImgData] = useState<ModifiedImageData>();
   const [reset, setReset] = useState<Boolean>();
 
   useEffect(() => {
+    // Set the tile dimensions with the ENV setting
     setTileWidth(parseInt(stdTileWidth));
     setTileHeight(parseInt(stdTileHeight));
 
-    const canvas:HTMLCanvasElement | null = document.querySelector("#imageCanvas") ? document.querySelector("#imageCanvas") : null;
-    if(canvas) {
-      canvas.height = document.body.offsetHeight - 200;
-    }
+    // Get the canvas and set the height
+    const canvas:HTMLCanvasElement | null = document.querySelector("#imageCanvas");
+    canvas!.height = document.body.offsetHeight - 200;
 
     async function doRender() {
-      if(tileCountX && tileCountY) {
-        const tiles:any = await getTiles(tileCountX, tileCountY);
-        drawTiles(tiles);
-      }
+      const tiles:ModifiedImageData[][] = await getTiles(tileCountX!, tileCountY!);
+      drawTiles(tiles!);
     }
 
     doRender();
@@ -50,14 +63,9 @@ const ImageUpload: React.FC<Prop> = ({ stdTileWidth, stdTileHeight }) => {
   useEffect(() => {
     // Reset the whole component if reset is true
     if(reset === true) {
-      console.log('resetting');
-      const canvas:HTMLCanvasElement | null = document.querySelector("#imageCanvas") ? document.querySelector("#imageCanvas") : null;
-      if(canvas) {
-        const ctx:CanvasRenderingContext2D | null = canvas.getContext("2d") ? canvas.getContext("2d") : null;
-        if(ctx) {
-          ctx.clearRect(0, 0, canvas.width, canvas.height);
-        }
-      }
+      const canvas:HTMLCanvasElement | null = document.querySelector("#imageCanvas");
+      const ctx:CanvasRenderingContext2D | null = canvas!.getContext("2d");
+      ctx!.clearRect(0, 0, canvas!.width, canvas!.height);
 
       // Reset all states to original states
       setImgUploaded(false);
@@ -74,7 +82,6 @@ const ImageUpload: React.FC<Prop> = ({ stdTileWidth, stdTileHeight }) => {
   }, [reset]);
 
   const handleImageUpload = (e: React.FormEvent<HTMLInputElement>) => {
-    console.log('uploading...');
     
     const files = (e.target as HTMLInputElement).files;
     if(files) {
@@ -98,94 +105,88 @@ const ImageUpload: React.FC<Prop> = ({ stdTileWidth, stdTileHeight }) => {
   }
 
   const init = (img:HTMLImageElement) => {
-    console.log('init');
+    // Get the canvas
     const canvas:HTMLCanvasElement | null = document.querySelector("#imageCanvas") ? document.querySelector("#imageCanvas") : null;
     const container = document.querySelector(".uploadContainer") as HTMLCanvasElement;
 
-    if (canvas) {
-      canvas.width = container.offsetWidth;
-      canvas.height = document.body.offsetHeight - 200;
+    // Make the canvas fill its parent container
+    canvas!.width = container.offsetWidth;
+    canvas!.height = document.body.offsetHeight - 200;
 
-      const ctx:CanvasRenderingContext2D | null = canvas.getContext("2d") ? canvas.getContext("2d") : null;
-      const scale = Math.min(canvas.width / img.width, canvas.height / img.height);
-      // Need to ensure that the tile is divisible by 4
-      const w = Math.round(img.width * scale);
-      const h = Math.round(img.height * scale);
+    // Get the canvas data
+    const ctx:CanvasRenderingContext2D | null = canvas!.getContext("2d") ? canvas!.getContext("2d") : null;
+    const scale = Math.min(canvas!.width / img.width, canvas!.height / img.height);
+
+    // Need to ensure that the tile is divisible by 4
+    const w = Math.round(img.width * scale);
+    const h = Math.round(img.height * scale);
+  
+    // Center the image in the canvas
+    const left = canvas!.width / 2 - w / 2;
+    const top = canvas!.height / 2 - h / 2;
+
+    // Calculate the amount of tiles
+    setTileCountX(~~(w / tileWidth!));
+    setTileCountY(~~(h / tileHeight!));
+
+    // Globaly set the new img dimensions
+    setImgW(w);
+    setImgH(h);
     
-      const left = canvas.width / 2 - w / 2;
-      const top = canvas.height / 2 - h / 2;
-    
-      if(tileWidth && tileHeight) {
-        setTileCountX(~~(w / tileWidth));
-        setTileCountY(~~(h / tileHeight));
-      }
-      setImgW(w);
-      setImgH(h);
-    
-      if(ctx) {
-        // ctx.drawImage(img, 0, 0);
-        ctx.drawImage(img, left, top, w, h);
-        setImgData(ctx.getImageData(left, top, w, h));
-        ctx.clearRect(left, top, canvas.width, canvas.height);
-      }
-    }
+    // Draw the image - set the image data - clear the canvas
+    ctx!.drawImage(img, left, top, w, h);
+    setImgData(ctx!.getImageData(left, top, w, h));
+    ctx!.clearRect(left, top, canvas!.width, canvas!.height);
   }
   
   //generate all tiles
   const getTiles = async (tileCountX:number, tileCountY:number) => {
-    console.log('getting tiles');
     const tiles = [];
+
     // Ensure all needed variables are not undefined
-    if(tileCountY && tileCountX && tileWidth && tileHeight) {
-      for (let yi = 0; yi < tileCountY; yi++) {
-        for (let xi = 0; xi < tileCountX; xi++) {
-          tiles.push(await getTile(xi * tileWidth, yi * tileHeight));
-        }
+    for (let yi = 0; yi < tileCountY; yi++) {
+      for (let xi = 0; xi < tileCountX; xi++) {
+        tiles.push(await getTile(xi * tileWidth!, yi * tileHeight!));
       }
     }
+
     return tiles;
   }
   
-  //get a tile of size tileWidth*tileHeight from position xy
+  // Get a tile of size tileWidth and tileHeight from position xy
   const getTile = async (x:number, y:number) => {
-    console.log('getTile');
-    // TODO: Need to change type from any
-    let tile:any = [];
-    const data:[] = imgData.data;
-
-    console.log(data);
+    let tile:ModifiedImageData[] = [];
+    const data:number[] = imgData!.data;
     
-    if(tileWidth) {
-      for (let i = 0; i < tileWidth; i++) {
-        await tile.push(...data.slice(getIndex(x, y + i), getIndex(x + tileWidth, y + i)));
-      }
-      //convert back to typed array and to imgdata object
-      tile = new ImageData(new Uint8ClampedArray(tile), tileWidth, tileHeight);
-
-      //save original position
-      tile.x = x;
-      tile.y = y;
-      
-      return tile;
+    for (let i = 0; i < tileWidth!; i++) {
+      await tile.push(...data.slice(getIndex(x, y + i), getIndex(x + tileWidth!, y + i)));
     }
+    // Convert back to typed array and to imgdata object
+    tile = new ImageData(new Uint8ClampedArray(tile), tileWidth!, tileHeight!);
+
+    // Save original tile position to array
+    tile.x = x;
+    tile.y = y;
+    
+    return tile;
   }
   
   const getIndex = (x:number, y:number) => {
-    const i = indexX(x) + indexY(y);
-    if (i > imgData.length) console.warn("XY out of bounds");
+    const i = indexX(x) + indexY(y)!;
+    if (i > imgData!.length) console.warn("XY out of bounds");
     return i;
   }
   
   //get imgdata index from img px positions
   const indexX = (x:number) => {
     const i = x * 4;
-    if (i > imgData.length) console.warn("X out of bounds");
+    if (i > imgData!.length) console.warn("X out of bounds");
     return i;
   }
   const indexY = (y:number) => {
     if(imgW) {
       const i = imgW * 4 * y;
-      if (i > imgData.length) console.warn("Y out of bounds");
+      if (i > imgData!.length) console.warn("Y out of bounds");
       return i;
     }
   }
@@ -193,47 +194,80 @@ const ImageUpload: React.FC<Prop> = ({ stdTileWidth, stdTileHeight }) => {
   const drawTiles = async (tiles:[]) => {
     // Get all of the SVG data
     const svgArray:SvgArray[] = await fetchSvgs(tiles);
-  
-    const canvas:HTMLCanvasElement | null = document.querySelector("#imageCanvas") ? document.querySelector("#imageCanvas") : null;
-    if (canvas) {
-      const ctx:CanvasRenderingContext2D | null = canvas.getContext("2d") ? canvas.getContext("2d") : null;
-      if (ctx) {
-        // Get the returned data and convert to a usable HTML Object
-        for (let i = 0; i < svgArray.length; i++) {
-          const svgObject = new DOMParser().parseFromString(svgArray[i].svg, "text/xml");
-          const svgURL = new XMLSerializer().serializeToString(svgObject);
-      
-          let url = window.btoa(svgURL);
-          url = 'data:image/svg+xml;base64,' + url;
-      
-          // Convert the SVG code to an img
-          const svgImage:HTMLImageElement = new Image();
-          svgImage.src = url;
+    
+    // Get the canvas data
+    const canvas:HTMLCanvasElement | null = document.querySelector("#imageCanvas");
+    const ctx:CanvasRenderingContext2D | null = canvas!.getContext("2d");
 
-          if(i === 0) {
-            setLoading(false);
-          }
-      
-          // Need timeout to place one tile at a time
-          setTimeout(() => {
-            ctx.drawImage(svgImage, svgArray[i].x, svgArray[i].y, tileWidth, tileHeight);
-          }, 5 * i);
-        }
-      }
+    let svgImgArr:SvgImage[] = [];
+
+    // Get the returned data and convert to a usable HTML Object
+    for (let i = 0; i < svgArray.length; i++) {
+      // Get the SVG string and make it useable
+      const svgObject = new DOMParser().parseFromString(svgArray[i].svg, "text/xml");
+      const svgURL = new XMLSerializer().serializeToString(svgObject);
+  
+      // Convert the SVG to Base64
+      let url = window.btoa(svgURL);
+      url = 'data:image/svg+xml;base64,' + url;
+  
+      // Convert the SVG code to an img
+      const svgImage:SvgImage = new Image();
+      svgImage.src = url;
+      svgImage.svgX = svgArray[i].x;
+      svgImage.svgY = svgArray[i].y;
+
+      svgImgArr.push(svgImage);
     }
+
+    setLoading(false);
+    const svgRows = getSvgRows(svgImgArr);
+    console.log(svgRows);
+
+    svgRows.forEach((row) => {
+      // Need this timeout in order to avoid the render from going too fast and missing out of tiles for the very first render
+      // Tested on Firefox fresh browser with no cache --- Found it requires 1000 timeout
+      setTimeout(() => {
+        row.forEach((item) => {
+            ctx!.drawImage(item, item.svgX!, item.svgY!, tileWidth!, tileHeight!);
+        })
+      }, 1000);
+    });
   }
 
-  async function fetchSvgs (tiles:any) {
+  const getSvgRows = (svgImgArr:SvgImage[]) => {
+    let resultArr:SvgImage[][] = [];
+    let row:number = 0;
+    let arrNum = 0;
+
+    for(let i:number = 0;i < svgImgArr.length!;i++) {
+      // Initalise the first row in the array
+      if(i === 0) {
+        resultArr[arrNum] = [];
+      }
+      // Check if the row exists and also if the SVG is in the same row as the previous
+      if(svgImgArr[i].svgY === row){
+        resultArr[arrNum].push(svgImgArr[i]);
+      } else {
+        arrNum++;
+        row = row + tileWidth!;
+        resultArr[arrNum] = [];
+        resultArr[arrNum].push(svgImgArr[i]);
+      }
+    }
+    return resultArr;
+  }
+
+  async function fetchSvgs (tiles:ImageData[]) {
     let colour = '';
     let svgArray:SvgArray[] = [];
-    console.log('tiles', tiles);
     for (const d of tiles) {
       // Get the RGB value of the tile
       colour = ((1 << 24) + (d.data[0] << 16) + (d.data[1] << 8) + d.data[2]).toString(16).slice(1);
 
       // Call the API to get the coloured SVG and store the value on an array
-      const data:any = await fetch(`http://localhost:3000/api/colour/${colour}`, {cache: "force-cache", credentials: 'same-origin'});
-      const dataResult:any = await data.text();
+      const data:Response = await fetch(`http://localhost:3000/api/colour/${colour}`, {cache: "force-cache", credentials: 'same-origin'});
+      const dataResult:string = await data.text();
       svgArray.push({svg:dataResult, x: d.x, y: d.y });
     };
     
